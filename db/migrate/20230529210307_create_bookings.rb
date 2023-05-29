ea52537bcd98d85b5c5aa6c4b38231d9fca4122e
Dennis WoldMcGimsey <denniswoldmcg@gmail.com>
2023-05-29 22:04:47 +0100

    Added Booking Model, created foreign key references

---
+++ db/migrate/20230529210307_create_bookings.rb
@@ -0,0 +1,11 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.boolean :approved
+      t.references :user, null: false, foreign_key: true
+      t.references :flat, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_150745) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_210307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.boolean "approved"
+    t.bigint "user_id", null: false
+    t.bigint "flat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "address"
@@ -23,6 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_150745) do
     t.string "guests_allowed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_150745) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "flats", "users"
 end
