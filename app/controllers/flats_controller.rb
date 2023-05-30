35ddcf25c1db0d70adce63a36dd9228acfb1e11b
Mateo Larbodiere <mateo.larbodiere@edhec.com>
2023-05-30 13:10:52 +0200

    Create the new function with the form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,4 +4,24 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def new
+    @flat = Flat.new # Needed to instantiate the form_with
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat, notice: "Restaurant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:address, :description, :price_per_day, :photo)
+  end
+
+
 end
