c683f8ce7b7949516e3befd4cf46a2e4e8c14159
Mateo Larbodiere <mateo.larbodiere@edhec.com>
2023-05-31 12:43:33 +0200

    we made change with andre

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -37,7 +37,7 @@ class FlatsController < ApplicationController
   private
 
   def flat_params
-    params.require(:flat).permit(:address, :description, :price_per_day, :guests_allowed)
+    params.require(:flat).permit(:address, :description, :price_per_day, :guests_allowed, :photo)
   end
 
 end
