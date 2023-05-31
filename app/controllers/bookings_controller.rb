e6e21e9c58a86b3bdf46f46266736a405d61bef4
Dennis WoldMcGimsey <denniswoldmcg@gmail.com>
2023-05-31 06:45:42 +0100

    Added grid boxes for flats on home/index page, added env with keys,

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,36 @@
-class BookingsController < ApplicationController
-end
+# class BookingsController < ApplicationController
+
+
+
+#   def my_bookings
+#     @bookings = Booking.where(user_id: current_user.id)
+#   end
+
+#   def new
+#     @booking = Booking.new # Needed to instantiate the form_with
+#   end
+
+#   def create
+#     @booking = Booking.new(flat_params)
+#     @booking.user = current_user
+
+#     if @booking.save
+#       redirect_to @flat, notice: "Booking was successfully created."
+#     else
+#       render :new, status: :unprocessable_entity
+#     end
+#   end
+
+#   def destroy
+#     @booking = Booking.find(params[:id])
+#     @booking.destroy
+#     redirect_to flats_path(@flat), notice: "Booking was successfully cancelled."
+#   end
+
+#   private
+
+#   def flat_params
+#     params.require(:flat).permit(:address, :description, :price_per_day, :guests_allowed)
+#   end
+
+# end
