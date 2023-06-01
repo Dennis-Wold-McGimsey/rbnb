c1edc4a77ec45c605f3ae1e697769188707af130
Dennis WoldMcGimsey <denniswoldmcg@gmail.com>
2023-06-01 04:50:39 +0100

    Added two booking columns, added ability to create booking, added ability to view my  bookings page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "flats#index"
 
-  resources :flats
+  resources :flats do
+    resources :bookings
+  end
+
+  get "my_bookings", to: "bookings#my_bookings"
+
   get "my_flats", to: "flats#my_flats"
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
