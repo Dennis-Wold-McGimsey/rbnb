11025ce5f4cd821215747d9ece965677e6db209f
Chris Terlegas <c.terlegas@gmail.com>
2023-06-02 15:15:12 +0100

    map finished

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -6,7 +6,7 @@ class FlatsController < ApplicationController
     @markers = @flats.geocoded.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
         info_window_html: render_to_string(partial: "info_window", locals: {flat: flat})
       }
     end
